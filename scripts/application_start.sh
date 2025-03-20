75bc413e1c53f2e4aefa02e31e08cc09b45c0743
hitasnhidarji <hitanshidarji95@gmail.com>
2025-03-20 16:55:12 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,3 @@
 echo "Running container..."
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 724772048206.dkr.ecr.eu-north-1.amazonaws.com
 docker run --name flask_app -d -p 5000:5000 724772048206.dkr.ecr.eu-north-1.amazonaws.com/flask_image:latest
